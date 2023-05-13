efd60c051aacbbdf5b9f4000a1fe4df5acc73545
Mandal Dev <mandal.dev@foxmail.com>
2023-05-14 00:10:10 +0800

    refine image dirs

--- js/defer.js
+++ js/defer.js
@@ -1,3 +1,18 @@
+function dirname(url) {
+	let end = url.lastIndexOf('/')
+	let start = url.lastIndexOf('/', end - 1)
+	return url.substr(start + 1, end - start - 1)
+}
+
+var imgs = document.getElementsByTagName('img')
+for (let img of imgs) {
+	let src = img.attributes.src.nodeValue
+	if (src.length > 0 && src[0] != '/') {
+		let realsrc = '/image/' + dirname(img.baseURI) + '/' + src
+		img.attributes.src.nodeValue = realsrc
+	}
+}
+
 var figcaps = document.getElementsByTagName('figcaption')
 for (let figcap of figcaps) {
 	let figure = figcap.parentElement
